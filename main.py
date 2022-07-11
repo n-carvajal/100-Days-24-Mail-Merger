69c6b9b7cef9dc9e88ca1ab555926fe5e3b3601d
Nicolas Carvajal <me@nicolascarvajal.com>
2022-07-11 17:27:11 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+with open("Input/Names/invited_names.txt") as name_file:
+    for name in name_file.readlines():
+        stripped_name = name.rstrip()
+        greeting = f"Dear {stripped_name},\n"
+        with open("Input/Letters/starting_letter.txt") as text_file:
+            text_list = text_file.readlines()
+            text_list[0] = greeting
+            with open(f"Output/ReadyToSend/letter_to_{stripped_name}.txt", mode="w") as letter_personalised:
+                letter_personalised.writelines(text_list)
+
+
 # TODO: Create a letter using starting_letter.txt
+
 # for each name in invited_names.txt
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
